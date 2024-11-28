9d15adcec4a0dfe1961a058ff67929a90a180755
AndresS2100 <90879362+AndresS2100@users.noreply.github.com>
2024-11-27 20:09:25 -0600

    Vista index / aboutus
    Se agrego la vista index y la de aboutus

---
+++ src/main/java/com/letseat/Controller/NosotrosController.java
@@ -0,0 +1,16 @@
+package com.letseat.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NosotrosController {
+    
+    @RequestMapping("/aboutus")
+    
+    public String paginaNostros(Model model){
+        return "aboutus";
+    }
+    
+}
